307a4ef9bbe89ef677e9319ce5bcb590c926504f
Denny Setyawan Huda <denycam9@gmail.com>
2023-05-15 13:18:31 +0700

    fix: adjust container

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -5,5 +5,5 @@ interface Children {
 }
 
 export default function Container({ children }: Children) {
-  return <div className={`mx-auto container md:px-24 hp:px-0 hp:relative  h-[100vh] overflow-x-hidden`}>{children}</div>;
+  return <div className="mx-auto container md:px-20">{children}</div>;
 }
