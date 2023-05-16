b5001b338fe5f7b6213aa59e69d9fac35abfc109
Denny Setyawan Huda <denycam9@gmail.com>
2023-05-16 12:55:21 +0700

    feat: add responsive mobile

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,32 +1,46 @@
-import heroPicture from '../../assets/images/hero-image.png'
+import heroPicture from "../../assets/images/hero-image.png";
 
 export default function Hero() {
-    return (
-        <article className="flex my-16">
-            <div className="hero-text">
-                <h1 className="font-semibold text-4xl">New <span className="text-[#4F7DF3]">Language</span>, new <br /> <span className="text-[#4F7DF3]">oppportunities</span>, new you </h1>
-                <p className="mt-4 text-base">Get access to compact lessons from the experts and connect with a community of native speakers to help you master words faster.</p>
-
-                <div className="mt-12 space-x-8 flex items-center">
-                    <button className="mr-10 h-10 px-6 font-semibold rounded-md bg-[#4475F2] text-white">Lets Join!</button>
-                    <div className="total-students flex-col text-center">
-                        <p className="font-semibold">100<span className="text-[#4F7DF3]">K+</span></p>
-                        <p className="font-base">Students</p>
-                    </div>
-                    <div className="total-classes flex-col text-center">
-                        <p className="font-semibold">20<span className="text-[#4F7DF3]">+</span></p>
-                        <p className="font-base">Students</p>
-                    </div>
-                    <div className="total-mentors flex-col text-center">
-                        <p className="font-semibold">24<span className="text-[#4F7DF3]">+</span></p>
-                        <p className="font-base">Mentors</p>
-                    </div>
-                </div>
-               
+  return (
+    <article className="flex items-center flex-col md:flex-row justify-between my-16 gap-10">
+      <div className="hero-text order-2 md:order-none md:w-1/2">
+        <h1 className="font-semibold text-center md:text-left text-3xl md:text-5xl leading-normal">
+          New <span className="text-primary">Language</span>, new <br />{" "}
+          <span className="text-primary">oppportunities</span>, new you{" "}
+        </h1>
+        <p className="mt-4 text-center md:text-left md:text-xl">
+          Get access to compact lessons from the experts and connect with a
+          community of native speakers to help you master words faster.
+        </p>
+        <div className="my-4 space-y-5 md:space-x-8 flex md:flex-row flex-col md:items-center">
+          <button className="py-3 px-10 font-semibold rounded-md bg-primary text-white">
+            Lets Join!
+          </button>
+          <div className="flex gap-5 justify-center">
+            <div className="total-students text-center">
+              <p className="font-semibold text-xl">
+                100<span className="text-primary">K+</span>
+              </p>
+              <p className="font-base">Students</p>
             </div>
-            <div className="hero-image">
-                <img src={heroPicture} />
+            <div className="total-classes text-center">
+              <p className="font-semibold text-xl">
+                20<span className="text-primary">+</span>
+              </p>
+              <p className="font-base">Students</p>
             </div>
-        </article>
-    )
+            <div className="total-mentors text-center">
+              <p className="font-semibold text-xl">
+                24<span className="text-primary">+</span>
+              </p>
+              <p className="font-base">Mentors</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="hero-image order-1 md:order-none md:w-1/2">
+        <img className="w-full" src={heroPicture} />
+      </div>
+    </article>
+  );
 }
