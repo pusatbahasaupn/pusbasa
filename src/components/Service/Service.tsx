a0f153ea41daa38ad24ec0f373085427db0dc27e
Denny Setyawan Huda <denycam9@gmail.com>
2023-05-16 13:28:12 +0700

    feat: service responsive

--- src/components/Service/Service.tsx
+++ src/components/Service/Service.tsx
@@ -5,7 +5,7 @@ export default function Service() {
   return (
     <div>
       <Title sub="service we provide" title="👥 • Pelayan yang kami berikan" />
-      <div className="flex  gap-5">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-5 ">
         <ServiceCard />
         <ServiceCard />
         <ServiceCard />
