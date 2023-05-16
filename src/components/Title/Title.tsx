06bef13db793cf395f6c7ae62c04f97d298938d9
Denny Setyawan Huda <denycam9@gmail.com>
2023-05-16 12:58:52 +0700

    feat: chnage font size

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -7,7 +7,7 @@ export default function Title({ sub, title }: ITitle) {
   return (
     <div className="my-10">
       <div className="uppercase text-primary font-semibold text-xl">{sub}</div>
-      <div className="text-5xl font-semibold">{title}</div>
+      <div className="text-4xl font-semibold">{title}</div>
     </div>
   );
 }
